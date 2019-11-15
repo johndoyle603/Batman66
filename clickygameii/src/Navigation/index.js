7f12c80b82074275b9bfa0ddc110a50dacdd4802
John Doyle <johndoyle@gmail.com>
2019-11-15 14:57:57 -0500

    success

--- clickygameii/src/Navigation/index.js
+++ clickygameii/src/Navigation/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Navigation.css";
 
-function Navigation() {
+function Navigation(props) {
   return (
 
     <div>
@@ -14,10 +14,10 @@ function Navigation() {
           <h2>Batman click game</h2>
         </div>
         <div className="col-md-3 label">
-          <h2>Current score:</h2>
+  <h2>Current score:{props.score}</h2>
         </div>
         <div className="col-md-3 label">
-          <h2>Top score: </h2>
+          <h2>Top score:{props.topScore} </h2>
         </div>
       </div>
     </div>
