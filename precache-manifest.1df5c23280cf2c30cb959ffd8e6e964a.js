11d47c3f10afe71d6f1c7170be1f345fe4aaa5ef
John Doyle <johndoyle@gmail.com>
2019-11-18 19:12:25 -0500

    Updates

---
+++ precache-manifest.1df5c23280cf2c30cb959ffd8e6e964a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c573b77c783eb7d020cc4a8e03f928f6",
+    "url": "/clickygame2/index.html"
+  },
+  {
+    "revision": "8bfc5775e6d7ace845e7",
+    "url": "/clickygame2/static/css/main.1f2080db.chunk.css"
+  },
+  {
+    "revision": "4804e15ef5686fdff05a",
+    "url": "/clickygame2/static/js/2.1d1fa761.chunk.js"
+  },
+  {
+    "revision": "8bfc5775e6d7ace845e7",
+    "url": "/clickygame2/static/js/main.7ba82750.chunk.js"
+  },
+  {
+    "revision": "013f48f1e0536799546a",
+    "url": "/clickygame2/static/js/runtime-main.515d5284.js"
+  }
+]);
